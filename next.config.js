8320698343252b3dfab583e1639712aed64b7d37
mrudulkolambe <mrudulkolambe02@gmail.com>
2022-03-05 13:23:50 +0530

    Bugs Solved

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  head: {
+    script: [
+        {
+            src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+            'data-ad-client': "9362505928898272",
+            async: true,
+        },
+    ],
+},
 }
--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return(
     <>
     <Script defer async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9362505928898272"
-     crossorigin="anonymous"></Script>
+     crossorigin="anonymous" data-ad-client="ca-pub-9362505928898272"></Script>
      <Component {...pageProps} />
     </>
      
